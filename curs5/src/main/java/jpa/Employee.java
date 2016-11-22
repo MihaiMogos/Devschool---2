bbae52404fd3223d3bffb29e8c0078bf37856878
mihaimogos <mihai.mogos@gmail.com>
2016-11-22 22:10:57 +0200

    curs 5 - jax-rs , ejb , jpa, jsf

--- curs5/src/main/java/jpa/Employee.java
+++ curs5/src/main/java/jpa/Employee.java
@@ -1,7 +1,9 @@
 package jpa;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Tolerate;
 
 import javax.persistence.*;
 
@@ -9,8 +11,11 @@ import javax.persistence.*;
  * Created by Mucefix on 22/11/16.
  */
 @Entity
-@Getter @Setter
+@Getter @Setter @Builder
 public class Employee {
+
+    @Tolerate
+    public Employee(){}
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ curs5/src/main/java/jsf/model/EmployeeBean.java
@@ -0,0 +1,41 @@
+package jsf.model;
+
+import ejb.EmployeeFacade;
+import jpa.Employee;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Mucefix on 22/11/16.
+ */
+@ManagedBean(name="employeeBean")
+@SessionScoped
+public class EmployeeBean implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    private static final List<Employee> employees = Arrays.asList(
+            Employee.builder().id(1).name("John Smith").salary(100).build(),
+            Employee.builder().id(2).name("Donald Trump").salary(200).build(),
+            Employee.builder().id(3).name("Hilary Clinton").salary(400).build(),
+            Employee.builder().id(4).name("James Gosling").salary(2000).build(),
+            Employee.builder().id(5).name("Venkat Subramaniam").salary(10000).build()
+
+    );
+
+    @EJB
+    private EmployeeFacade employeeFacade;
+
+    public List<Employee> getEmployees() {
+        //return employees;
+        return employeeFacade.getEmployees();
+
+    }
+
+
+}
