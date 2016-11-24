a73451b2004b7905ea9c3f152b887f8274feb1e8
AndreiB123 <andreii.bumbu@gmail.com>
2016-11-24 18:44:07 +0200

    Bean

---
+++ curs6-inclass/src/main/java/model/HelloWorld.java
@@ -0,0 +1,28 @@
+package model;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "helloWorld")
+@RequestScoped
+public class HelloWorld {
+
+    @ManagedProperty(value="#{message}")
+    private Message messageBean;
+
+    private String message;
+
+    public HelloWorld() {
+        System.out.println("Hello JSF World");
+    }
+    public String getMessage() {
+        if(messageBean != null){
+            message = messageBean.getMessage();
+        }
+        return message;
+    }
+    public void setMessageBean(Message message) {
+        this.messageBean = message;
+    }
+}
